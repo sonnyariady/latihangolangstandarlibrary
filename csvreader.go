ff871e176088a18d9a5d22365eb374e7d1735451
Sonny Ariady <sonnyariady@gmail.com>
2024-08-01 11:50:31 +0700

    pembelajaran csvreader

--- csvreader.go
+++ csvreader.go
@@ -3,6 +3,7 @@ package main
 import (
 	"encoding/csv"
 	"fmt"
+	"io"
 	"strings"
 )
 
@@ -11,26 +12,41 @@ type Alamat struct {
 }
 
 func main() {
-	datacsv := "Nama,Alamat,Telp"
+	var daftaralamats []Alamat
+	datacsv := "Nama,Alamat,Telp\n"
 	datacsv = datacsv + "Didi,Jl.Mangga,123456\n"
 	datacsv = datacsv + "Agus,Jl.Apel,6666\n"
 	datacsv = datacsv + "Tono,Jl.Pegangsaan,4444\n"
 	fmt.Println(datacsv)
 
 	reader := csv.NewReader(strings.NewReader(datacsv))
-	for {
+
+	for idx := 0; ; idx++ {
 		records, err := reader.Read()
+		if err == io.EOF {
+			break
+		}
+		if idx == 0 {
+			continue
+		}
+		/*
+			fmt.Println("Index: ", idx)
+			fmt.Println(records)
+			fmt.Println("Pecah record")
+			fmt.Println("Nama : ", records[0])
+			fmt.Println("Alamat : ", records[1])
+			fmt.Println("Telp : ", records[2])
+		*/
+		daftaralamats = append(daftaralamats, Alamat{records[0], records[1], records[2]})
+
+		//masukin ke slice of struct
+
 	}
 
-	// Membaca semua data dari file CSV
-	records, err := reader.ReadAll()
-	if err != nil {
-		fmt.Println("Error:", err)
-		return
-	}
+	fmt.Println("Cetak slice daftar alamat")
+	fmt.Println(daftaralamats)
 
-	// Menampilkan data yang telah dibaca
-	for _, record := range records {
-		fmt.Println(record)
+	for idx, itm := range daftaralamats {
+		fmt.Printf("Data ke-%d --> Nama : %s, Alamat : %s, Telp : %s\n", idx, itm.Alamat, itm.Alamat, itm.Telp)
 	}
 }
